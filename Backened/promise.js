b928d0ce6003dcf5feee474b802818ed4aaa6c05
Rohit Gupta <rohitharidyal620@gmail.com>
2026-09-03 11:18:27 +0530

    promise1

--- Backened/promise.js
+++ Backened/promise.js
@@ -1,17 +1,36 @@
-let promise = new Promise((resolve, reject) => {
-    console.log("I am a new Promise");
+// let promise = new Promise((resolve, reject) => {
+//     console.log("I am a new Promise");
 
-    // resolve("Successfully executed");
-    reject("Reject your request");
+//     // resolve("Successfully executed");
+//     reject("Reject your request");
+// });
+
+// function GetData(dataId) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("data", dataId);
+//            // resolve("Successful");
+//             reject("error");
+//         }, 9000);
+//     });
+// }
+
+// let r = GetData(123);
+
+const Getpromise = () => {
+    return new Promise((resolve, reject) => {
+        console.log("I am a promise");
+        //resolve("Successful");
+         reject("network error");
+    });
+};
+
+let promise = Getpromise();
+
+promise.then(() => {
+    console.log("promise is fulfilled");
 });
 
-function GetData(dataId) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log("data", dataId);
-            resolve("Successfull");
-        }, 8000);
-    });
-}
-
-let r = GetData(123);
+promise.catch(() => {
+    console.log("network is not working properly");
+});
