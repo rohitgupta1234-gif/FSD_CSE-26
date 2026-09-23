eefc897e5d75d830bb7b1f5b087d2776acb11faf
Rohit Gupta <rohitharidyal620@gmail.com>
2026-09-23 10:02:19 +0530

    post.js

---
+++ Exp-5.2-Post/post.js
@@ -0,0 +1,24 @@
+const express = require(`express`);
+const app=express();
+
+app.use(express.json());
+
+let students =[
+    {id:1,name:"Rahul", branch:"CSE"},
+    {id:2, name: "Aman", branch: "IT"}
+];
+
+app.get('/',(req,res)=>{
+    res.send("Server is running");
+});
+
+app.post('/students', (req,res)=>{
+    const newStudent = req.body;
+    students.push(newStudent);
+    res.status(201).json({message:"Student added successfully", student: newStudent})
+    
+});
+
+app.listen(3005,()=>{
+    console.log("Server running at port 3005 ");
+});
